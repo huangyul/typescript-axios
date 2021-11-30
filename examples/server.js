2e31f6cf6ee012e26f33d01df2db3c1f178ae7cf
yul <1103221645@qq.com>
2021-11-30 10:15:47 +0800

    Handing URL request parameters

--- examples/server.js
+++ examples/server.js
@@ -5,6 +5,18 @@ const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const WebpackConfig = require('./webpack.config')
 
+const router = express.Router()
+
+router.get('/simple/get', function(req, res) {
+  res.json({
+    msg: `hello world`
+  })
+})
+
+router.get('/base/get', function(req, res) {
+  res.json(req.query)
+})
+
 const app = express()
 const compiler = webpack(WebpackConfig)
 
@@ -25,7 +37,9 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 8080
+app.use(router)
+
+const port = 5555
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { AxiosRequestConfig } from './types'
+import {buildURL} from './helper/url'
 
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+/**
+ * 处理url参数
+ * @param config 
+ */
+function transfromUrl(config: AxiosRequestConfig): string {
+  const {url, params} = config
+  return buildURL(url, params)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transfromUrl(config)
 }
 
 export default axios
