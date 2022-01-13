0ab9879ab51e246de7c4b119d4dc19168f23aced
yul <1103221645@qq.com>
2022-01-13 16:03:39 +0800

    获取响应数据

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,27 @@
+// 请求参数配置
 export type AxiosRequestConfig = {
   url: string // 请求地址
   method?: Method // 请求方法
   headers?: any // 请求头
   data?: any // 请求数据
   params?: any // url参数
+  responseType?: XMLHttpRequestResponseType
 }
+
+// 响应参数
+export type AxiosResponse = {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+/**
+ * resolve函数中的参数就是一个AxiosResponse类型
+ */
+export interface AxiosPromise extends Promise<AxiosResponse> {}
 
 // 规范方法的参数
 export type Method =
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,24 +1,48 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
 
 /**
  * 默认请求方法
  * @param config 请求配置
  */
-export default function xhr(config: AxiosRequestConfig) {
-  const { data = null, url, method = 'get', headers } = config
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise(resolve => {
+    const { data = null, url, method = 'get', headers, responseType } = config
 
-  const request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
 
-  request.open(method.toUpperCase(), url, true)
-
-  Object.keys(headers).forEach(name => {
-    // 根据
-    if (data === null && name.toLowerCase() === 'content-type') {
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+    if (responseType) {
+      request.responseType = responseType
     }
-  })
 
-  request.send(data)
+    request.open(method.toUpperCase(), url, true)
+
+    request.onreadystatechange = function headleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+      const responseHeaders = request.getAllResponseHeaders()
+      const responseData =
+        responseType && responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response)
+    }
+
+    Object.keys(headers).forEach(name => {
+      // 根据
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.send(data)
+  })
 }
